f286a817b1409286279b94ee0b09df9f1a9041f8
Ilya Kushlianski <kushliansky@gmail.com>
2019-11-11 14:01:18 +0300

    Broadcast the typing feedback message to all chat participants

--- public/chat.js
+++ public/chat.js
@@ -4,14 +4,34 @@ const message = document.getElementById('message');
 const handle = document.getElementById('handle');
 const btn = document.getElementById('send');
 const output = document.getElementById('chat-output');
+const feedback = document.getElementById('feedback');
 
 btn.addEventListener('click', () => {
+  sendChatMessage(socket, handle, message);
+});
+
+document.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    sendChatMessage(socket, handle, message);
+  }
+});
+
+message.addEventListener('keypress', () => {
+  socket.emit('typing', handle.value);
+});
+
+socket.on('chat', (data) => {
+  output.innerHTML += `<p class="chat-message"><b>${data.handle}</b>: ${data.message}</p>`;
+  feedback.innerHTML = null;
+});
+
+socket.on('typing', (data) => {
+  feedback.innerHTML = `<p class="feedback">${data} is typing message...</p>`
+})
+
+function sendChatMessage(socket, handle, message) {
   socket.emit('chat', {
     message: message.value,
     handle: handle.value,
   })
-});
-
-socket.on('chat', (data) => {
-  output.innerHTML += `<p>${data.handle}: ${data.message}</p>`;
-});
+}
